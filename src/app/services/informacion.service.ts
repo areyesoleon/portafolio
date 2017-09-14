2c23efb6545e7ee82731ddcc906469263d92040f
Andres Reyes <areyes.oleon@gmail.com>
2017-09-14 11:41:10 -0600

    servicio de firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -3,14 +3,31 @@ import { Http } from '@angular/http';
 
 @Injectable()
 export class InformacionService {
-  public info:object = {};
+  public info: object = {};
+  public cargada: boolean = false;
+  public cargada_sobre_nosotros: boolean = false;
+  public equipo: any[] = [];
   constructor(
     public http: Http
   ) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info() {
     this.http.get("assets/data/info.pagina.json")
       .subscribe((data) => {
+        this.cargada = true;
         this.info = data.json();
+      })
+  }
+
+  public carga_sobre_nosotros() {
+    this.http.get("https://paginaweb-3ba96.firebaseio.com/equipo.json")
+      .subscribe((data) => {
         console.log(data.json());
+        this.cargada_sobre_nosotros = true;
+        this.equipo = data.json();
       })
   }
 
